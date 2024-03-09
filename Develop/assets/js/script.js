fd8f9116ca25c8cb643a534fd1f11de2035c1d2a
blebeuf <bryanlebeuf@gmail.com>
2024-03-08 18:23:58 -0600

    jQuery draggable function test

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -66,6 +66,43 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    const task = readTasksFromStorage();
+
+// Empty existing project cards out of the lanes
+    const todoList = $('#todo-cards');
+    todoList.empty();
+
+    const inProgressList = $('#in-progress-cards');
+    inProgressList.empty();
+
+    const doneList = $('#done-cards');
+    doneList.empty();
+
+// Loop through projects and create project cards for each status
+    for (let task of tasks) {
+        if (task.status === 'to-do') {
+          todoList.append(createTaskCard(task));
+        } else if (task.status === 'in-progress') {
+          inProgressList.append(createTaskCard(task));
+        } else if (task.status === 'done') {
+          doneList.append(createTaskCard(Task));
+        }
+    }
+    $('.draggable').draggable({
+        opacity: 0.7,
+        zIndex: 100,
+        // This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+        helper: function (e) {
+          // Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+          const original = $(e.target).hasClass('ui-draggable')
+            ? $(e.target)
+            : $(e.target).closest('.ui-draggable');
+          // Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+          return original.clone().css({
+            width: original.outerWidth(),
+          });
+        },
+      });
 
 }
 
